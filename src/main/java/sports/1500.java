e39240ead334867983b0db906e7ebad34fb79517
fayaz <rsfayaz86@gmail.com>
2021-02-04 22:47:40 +0530

    nike shoe

---
+++ src/main/java/sports/1500.java
@@ -0,0 +1,9 @@
+package sports;
+
+public class 1500 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sports/1500Test.java
@@ -0,0 +1,38 @@
+package sports;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class 1500Test 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public 1500Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( 1500Test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void test1500()
+    {
+        assertTrue( true );
+    }
+}
